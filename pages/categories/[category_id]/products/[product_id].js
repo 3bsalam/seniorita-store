1d48d87dfdcef3c3377eefe8405e18b1191e5f43
Mostafa <mostafa.abdelsalam96@gmail.com>
2024-03-20 01:55:05 +0100

    Changed routing for products

---
+++ pages/categories/[category_id]/products/[product_id].js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CommonLayout from '../../../../components/shop/common-layout';
+import LeftImagePage from './product/leftImagePage';
+// import { withApollo } from '../../helpers/apollo/apollo';
+import ProductTab from './common/product-tab';
+import ProductSection from './common/product_section';
+import { useRouter } from 'next/router'
+
+const ThumbnailLeft = () => {
+  const router = useRouter()
+  const category = router.query.category_id;
+  const product_id = router.query.product_id;
+    return (
+        <CommonLayout parent="home" title="product">
+            <LeftImagePage product_id={product_id} />
+            <ProductTab product_id={product_id} />
+            <ProductSection product_id={product_id} category={category} />
+        </CommonLayout>
+    )
+}
+
+export default ThumbnailLeft; 
